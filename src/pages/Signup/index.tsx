e7bddb46ce47a355d3cf9f5fdffb2062c5f13016
Yeongin Park <pyi7628@gmail.com>
2022-09-06 23:58:57 +0900

    feat: 유저 정보 유효성 체크 로직 추가

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, ChangeEvent } from 'react';
 
+import { isEmail, isPassword, isNickname, isSameStr } from '../../utils/shared';
+
 import Input from '../../components/Input';
 
 // type 어디다가 두고 관리할지 논의하기
@@ -13,11 +15,39 @@ type UserInfo = {
 
 const Signup = () => {
   const [userInfo, setUserInfo] = useState({} as UserInfo);
+  const [checkEmailFormat, setCheckEmailFormat] = useState(false as boolean);
+  const [checkPasswordFormat, setCheckPasswordFormat] = useState(
+    false as boolean
+  );
+  const [checkNicknameFormat, setCheckNicknameFormat] = useState(
+    false as boolean
+  );
 
   const handleInputChange = (e: ChangeEvent) => {
     const { name, value } = e.target as HTMLInputElement;
     setUserInfo({ ...userInfo, [name]: value });
   };
+
+  const submitUserInfo = () => {
+    console.log(
+      checkEmailFormat,
+      checkPasswordFormat,
+      checkNicknameFormat,
+      isSameStr(userInfo.password, userInfo.passwordCheck)
+    );
+
+    if (
+      checkEmailFormat &&
+      checkPasswordFormat &&
+      checkNicknameFormat &&
+      isSameStr(userInfo.password, userInfo.passwordCheck)
+    ) {
+      // submit
+    } else {
+      // 기입 정보 재확인
+    }
+  };
+
   return (
     <div className="signup-page">
       <div className="signup-info-wrapper">
@@ -26,7 +56,14 @@ const Signup = () => {
           <Input
             name="email"
             value={userInfo.email}
-            handleChange={(evt) => handleInputChange(evt)}
+            handleChange={(e) => {
+              if (isEmail(e.target.value)) {
+                setCheckEmailFormat(true);
+              } else {
+                setCheckEmailFormat(false);
+              }
+              handleInputChange(e);
+            }}
           />
         </div>
         <div className="signup-info-box">
@@ -35,7 +72,14 @@ const Signup = () => {
             name="password"
             value={userInfo.password}
             type="password"
-            handleChange={handleInputChange}
+            handleChange={(e) => {
+              if (isPassword(e.target.value)) {
+                setCheckPasswordFormat(true);
+              } else {
+                setCheckPasswordFormat(false);
+              }
+              handleInputChange(e);
+            }}
           />
         </div>
         <div className="signup-info-box">
@@ -44,7 +88,9 @@ const Signup = () => {
             name="passwordCheck"
             value={userInfo.passwordCheck}
             type="password"
-            handleChange={handleInputChange}
+            handleChange={(e) => {
+              handleInputChange(e);
+            }}
           />
         </div>
         <div className="signup-info-box">
@@ -52,12 +98,21 @@ const Signup = () => {
           <Input
             name="nickname"
             value={userInfo.nickname}
-            handleChange={handleInputChange}
+            handleChange={(e) => {
+              if (isNickname(e.target.value)) {
+                setCheckNicknameFormat(true);
+              } else {
+                setCheckNicknameFormat(false);
+              }
+              handleInputChange(e);
+            }}
           />
         </div>
       </div>
       <div className="signup-button-wrapper">
-        <button type="button">회원가입하기</button>
+        <button type="button" onClick={submitUserInfo}>
+          회원가입하기
+        </button>
       </div>
     </div>
   );
