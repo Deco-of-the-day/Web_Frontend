c05b71d42f69852404e9d5df698fd9d7af9b8583
Yeongin Park <pyi7628@gmail.com>
2022-09-07 00:47:53 +0900

    feat: 로그인 마크업 작업

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Signin() {
+  return (
+    <div className="signin-page">
+      <div className="signin-userinfo-wrapper">
+        <div className="signin-userinfo-box">
+          <input />
+        </div>
+        <div className="signin-userinfo-box">
+          <input />
+        </div>
+      </div>
+      <div className="signin-button-wrapper">
+        <button type="button">로그인</button>
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
