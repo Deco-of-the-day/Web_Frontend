b8bd828764acc47f2559c20e1b3604ef2c5f9ff9
Yeongin Park <pyi7628@gmail.com>
2022-09-21 22:59:52 +0900

    feat: 회원가입 api 바인딩

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,11 +1,10 @@
 import React, { useState, ChangeEvent } from 'react';
 
+import api from '../../api/api';
 import { isEmail, isPassword, isNickname, isSameStr } from '../../utils/shared';
 
 import Input from '../../components/Input';
 
-// type 어디다가 두고 관리할지 논의하기
-// 근데 type이랑 interface 차이는?
 type UserInfo = {
   email: string;
   password: string;
@@ -22,29 +21,46 @@ const Signup = () => {
   const [checkNicknameFormat, setCheckNicknameFormat] = useState(
     false as boolean
   );
+  const [isDuplicatedEmail, setIsDuplicatedEmail] = useState(true as boolean);
 
   const handleInputChange = (e: ChangeEvent) => {
     const { name, value } = e.target as HTMLInputElement;
     setUserInfo({ ...userInfo, [name]: value });
   };
 
-  const submitUserInfo = () => {
-    console.log(
-      checkEmailFormat,
-      checkPasswordFormat,
-      checkNicknameFormat,
-      isSameStr(userInfo.password, userInfo.passwordCheck)
-    );
-
+  const submitUserInfo = async () => {
     if (
       checkEmailFormat &&
       checkPasswordFormat &&
       checkNicknameFormat &&
-      isSameStr(userInfo.password, userInfo.passwordCheck)
+      isSameStr(userInfo.password, userInfo.passwordCheck) &&
+      !isDuplicatedEmail
     ) {
       // submit
+      const params = {
+        email: userInfo.email,
+        password: userInfo.password,
+        nickname: userInfo.nickname,
+      };
+
+      const { status } = await api.post(`/user`, params);
+      if (status === 200) {
+        // 회원가입 성공
+      } else {
+        // 회원가입 실패
+      }
     } else {
       // 기입 정보 재확인
+    }
+  };
+
+  const findUserInfo = async () => {
+    const { status } = await api.get(`/user/${userInfo.email}`, {});
+    if (status === 200) {
+      // 이미 있는 계정
+      setIsDuplicatedEmail(true);
+    } else {
+      setIsDuplicatedEmail(false);
     }
   };
 
@@ -65,6 +81,9 @@ const Signup = () => {
               handleInputChange(e);
             }}
           />
+          <button type="button" onClick={findUserInfo}>
+            중복확인
+          </button>
         </div>
         <div className="signup-info-box">
           <label>비밀번호</label>
